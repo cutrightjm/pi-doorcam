03a5d5802a8acffd49b764bf40554b64f5a5ea7c
Jacob Cutright <cutright_jm@wvwc.edu>
2019-07-10 23:23:57 -0400

    Update complete.py
    Fixed SCP command, changed path to /tmp, few cleanup items

--- complete.py
+++ complete.py
@@ -3,8 +3,6 @@ from gpiozero import Button         # Easily lets us use buttons
 from datetime import datetime       # For time and timezones
 from pytz import timezone           # For time and timezones
 import time                         # This allows the sleep function so everything times correct
-import threading                    # Allows backgrounding of the video recording process, which allows
-                                    # # # us to have accurate door closing times
 import subprocess                   # Enable threading of data offloading
 
 button = Button(4)
@@ -14,39 +12,53 @@ device_name = 'freezer_one'         #Define a friendly device name to easily ide
 record_time = 5                     #Define the number of seconds we want to record
 camera.resolution = (1920, 1080)    #Define the resolution we want the video to be at
 
-upload_server = '192.168.43.147'
-upload_user = 'root'
-privkey_location = '/home/pi/Desktop/id_rsa'
-upload_destination = '/root/video'
+isRecording = 0                     # Maintains state of recording operation
+close_length = 5                    # Record for <n> seconds after closing door
 
-def record_video(record_time):
-    print("Recording video at " + current_time)
+upload_server = '192.168.43.147'                                # Define the server to upload to
+upload_user = 'root'                                                    # Define a non-root user to log in as
+privkey_location = '/home/pi/Desktop/id_rsa'    # Add full path to private key for passwordless logon
+upload_destination = '/root/video'                              # Add upload destination on remote server
+upload_path = upload_user + '@' + upload_server + ':' + upload_destination
+
+def record_video():
+    global file_name
+    print("... Recording video at " + current_time)
     camera.start_preview()
-    file_name = '/home/pi/Desktop/' + device_name + current_time + '.h264'
+    file_name = '/tmp/' + device_name + current_time + '.h264'
+    print("... The file_name is set to " + file_name)
     camera.start_recording(file_name)
-    time.sleep(record_time)
-    camera.stop_recording()
-    camera.stop_preview()
-    scp_command = "scp -i " + privkey_location + " " + file_name + " " + upload_user + "@" + upload_server + ":" + upload_destination
-    print(scp_command)
-    subprocess.call([scp_command])
     return;
-
-def upload_video(file_name):
-    print file_name
 
 while True:
     current_time = datetime.now(timezone('US/Eastern')).strftime('%Y-%m-%d_%H-%M-%S')
     if button.is_pressed:
-        print("The door was opened at " + current_time)
-        #record_time = 5
-        print("Recording video for " + str(record_time) + " seconds")
-        background_thread = threading.Thread(target=record_video, args=[record_time])
-        background_thread.start()
-        time.sleep(1) # Do this so release message doesn't get jumbled
-        print("Waiting for door to close...")
-        button.wait_for_release()
+        if isRecording == 0:
+            print("The door was opened at " + current_time)
+            print("... Begin video recording")
+            record_video()
+            isRecording = 1
+            print("... Waiting for door to close")
+        else:
+            pass
 
     else:
-        print("The door was closed at " + current_time)
-        button.wait_for_press()
+        button.wait_for_release()
+        if isRecording == 1:
+            print("The door was closed at " + current_time)
+            isRecording = 0
+            time.sleep(close_length) # Record specified number of seconds after door closing
+            camera.stop_recording()
+            camera.stop_preview()
+            print("... Recording for an additional " + str(record_time) + " seconds")
+            time.sleep(record_time)
+            scp_command = ['scp', '-i', privkey_location, file_name, upload_path]
+            print("... Beginning data upload to " + upload_server)
+            subprocess.call(scp_command)
+            delete_command = ['rm', file_name]
+            print("... Deleting file " + file_name)
+            subprocess.call(delete_command)
+
+        else:
+            print("... Waiting for door to open")
+            button.wait_for_press()
